c08bcff0b26eb42b5d7ec8516f92a73bd75d27e1
Dany Fu <danyfu@bu.edu>
2020-10-16 17:56:05 -0400

    update qPCR prep protocol

--- qPCR_taqpath_multiplex/qPCR_taqpath_multiplex.py
+++ qPCR_taqpath_multiplex/qPCR_taqpath_multiplex.py
@@ -1,4 +1,4 @@
-# qPCR Assay Setup Protocol
+﻿# qPCR Assay Setup Protocol
 # Written By Rita Chen 2020-05-21
 # Updated by Dany Fu 2020-06-10
 
@@ -23,61 +23,150 @@
 # 6. Seal the plate with MicroAmp™ Optical Adhesive Film, vortex the plate for 10 seconds to ensure proper mixing,\
 # then centrifuge for 1 minute at 2000 rpm to collect the liquid at the bottom of the reaction plate.
 
+import math
 from opentrons import protocol_api
 metadata = {'apiLevel': '2.2',
-            'protocolName': 'RNA Extraction (MagMAX)',
+            'protocolName': 'qPCR Prep (MagMAX)',
             'author': 'Rita Chen, Dany Fu',
-            'description': '''Distributes master mix to 96 well plate'''}
+            'description': 'Aliquot RNA eluent and distribute master mix to 96 well plate'}
 
 TEMP_DECK = {
     'NAME': 'Temperature Module',
     'SLOT': 7
 }
-P200_MULTI = {
-    'NAME': 'p300_multi',
-    'POSITION': 'left'
+QPCR_PLATE = {#200uL per well, 96 wells
+    'NAME': 'biorad_96_wellplate_200ul_pcr',
+    'LABEL': 'Output Plate', #no slot, sits on temp deck
+}
+RNA_PLATE = { #200uL per well, 96 wells
+    'NAME': 'biorad_96_wellplate_200ul_pcr',
+    'LABEL': 'RNA Plate',
+    'SLOT': 8
+}
+REAGENT_PLATE = {
+    'NAME': 'biorad_96_wellplate_200ul_pcr',
+    'SLOT': 4,
+    'LABEL': 'Reagent Plate'
+}
+P10_MULTI = {
+    'NAME': 'p20_multi_gen2',
+    'POSITION': 'right'
 }
 FILTER_TIP_20 = [{
-    'NAME': 'opentrons_96_filtertiprack_200ul',
+    'NAME': 'opentrons_96_filtertiprack_20ul',
     'SLOT': 1,
     'LABEL': 'Filter Tip S-1'
+},
+{
+    'NAME': 'opentrons_96_filtertiprack_20ul',
+    'SLOT': 2,
+    'LABEL': 'Filter Tip S-2'
 }]
 
 ASPIRATE_DEPTH_BOTTOM = 2.00 #2mm from bottle
+VOL_RNA = 10
 VOL_MASTER_MIX = 15 # Reaction volume
 TEMP = 4
-TOUCH_HEIGHT = -2.0
 TOUCH_SPEED = 20.0
+TOUCH_RADIUS_SM_SM = 1.20
+TOUCH_HEIGHT_SM_SM = -2.0
 
 def run(protocol: protocol_api.ProtocolContext):
-	temp_deck = protocol.load_module(TEMP_DECK['NAME'], location=TEMP_DECK['SLOT'])
-	reaction_plate = temp_deck.load_labware('biorad_96_wellplate_200ul_pcr',
-											label='Reaction Plate')
-	reagent_plate = protocol.load_labware('biorad_96_wellplate_200ul_pcr',
-                                          location=4,
-										  label='Reagent Plate')
-	reagent_mix = reagent_plate.columns()[0]
-	tip_20 = [protocol.load_labware(i['NAME'], location=i['SLOT'], label=i['LABEL'])
+    temp_deck = protocol.load_module(TEMP_DECK['NAME'], location=TEMP_DECK['SLOT'])
+    qPCR_plate = temp_deck.load_labware(QPCR_PLATE['NAME'],
+                                       label=QPCR_PLATE['LABEL'])
+    rna_plate = protocol.load_labware(RNA_PLATE['NAME'],
+                                      location=RNA_PLATE['SLOT'],
+                                      label=RNA_PLATE['LABEL'])
+    reagent_plate = protocol.load_labware(REAGENT_PLATE['NAME'],
+                                          location=REAGENT_PLATE['SLOT'],
+                                          label=REAGENT_PLATE['LABEL'])
+    mastermix = reagent_plate.columns()[0]
+    tip_20 = [protocol.load_labware(i['NAME'], location=i['SLOT'], label=i['LABEL'])
               for i in FILTER_TIP_20]
-	p200 = protocol.load_instrument(P200_MULTI['NAME'], P200_MULTI['POSITION'],
+    p20 = protocol.load_instrument(P10_MULTI['NAME'], P10_MULTI['POSITION'],
 								    tip_racks=tip_20)
-	p200.well_bottom_clearance.aspirate = ASPIRATE_DEPTH_BOTTOM
-	p200.well_bottom_clearance.dispense = ASPIRATE_DEPTH_BOTTOM
+    p20.well_bottom_clearance.aspirate = ASPIRATE_DEPTH_BOTTOM
+    p20.well_bottom_clearance.dispense = ASPIRATE_DEPTH_BOTTOM
 
-	num_cols = len(reaction_plate.columns())
+    num_cols = len(qPCR_plate.columns())
 
-	temp_deck.set_temperature(celsius=TEMP)
-	add_master_mix(num_cols=num_cols, pipette=p200,
-				   source=reagent_mix, dest=reaction_plate.columns())
+    temp_deck.set_temperature(celsius=TEMP)
+    aliquot_eluent(num_cols=num_cols, pipette=p20,
+                   source=rna_plate.columns(), dest=qPCR_plate.columns())
+    add_master_mix(num_cols=num_cols, pipette=p20,
+                   source=mastermix, dest=qPCR_plate.columns())
+
+def aliquot_eluent(num_cols=1, pipette=None, source=None, dest=[]):
+    for c in range(num_cols):
+        transfer(vol=VOL_RNA, pipette=pipette,
+                 source=source[c], dest=dest[c])
 
 def add_master_mix(num_cols=1, pipette=None, source=None, dest=[]):
     # Transfer 15ul of reagent mix (reagent plate) to the pre-prepared reaction plate
     for c in range(num_cols):
-        pipette.pick_up_tip()
-        pipette.mix(repetitions=5, volume=VOL_MASTER_MIX, location=source[0])
-        pipette.aspirate(volume=VOL_MASTER_MIX, location=source[0])
-        pipette.dispense(volume=VOL_MASTER_MIX, location=dest[c][0])
-        pipette.mix(repetitions=5, volume=VOL_MASTER_MIX)
-        pipette.blow_out(location=dest[c][0])
-        pipette.touch_tip(v_offset=TOUCH_HEIGHT, speed=TOUCH_SPEED)
-        pipette.drop_tip()
+        transfer(vol=VOL_MASTER_MIX, pipette=pipette,
+                 source=source, dest=dest[c],
+                 mix_before=(5, VOL_MASTER_MIX), mix_after=(5, VOL_MASTER_MIX),
+                 touch_tip=(TOUCH_RADIUS_SM_SM, TOUCH_HEIGHT_SM_SM))
+
+# Custom transfer function; when the volume needed
+# exceeds the pipette's max volume this function will
+# prioritize tip reuse by pipetting to the top of the
+# wells until the last dispensation
+def transfer(vol=0, pipette=None, source=[], dest=[],
+             mix_before=None, mix_after=None,
+             touch_tip=None, delay_time_s=None):
+    n = math.ceil(vol / pipette.hw_pipette['working_volume'])
+    vol_ar = [vol // n + (1 if x < vol % n else 0) for x in range(n)]
+    pipette.pick_up_tip()
+
+    # dispense to the top of the well so we can reuse the tips
+    for v in vol_ar[:-1]:
+        if mix_before:
+            if len(mix_before) == 1:
+                pipette.mix(repetitions=mix_before[0],
+                            volume=v,
+                            location=source[0])
+            if len(mix_before) == 2:
+                pipette.mix(repetitions=mix_before[0],
+                            volume=mix_before[1],
+                            location=source[0])
+        pipette.aspirate(volume=v, location=source[0])
+        if(delay_time_s):
+            pipette.air_gap(volume=0)
+            protocol.delay(seconds=delay_time_s)
+        pipette.dispense(volume=v, location=dest[0].top())
+        pipette.blow_out(location=dest[0])
+        if touch_tip:
+            pipette.touch_tip(radius=touch_tip[0],
+                              v_offset=touch_tip[1],
+                              speed=TOUCH_SPEED)
+
+    # the final transfer
+    if mix_before:
+        if len(mix_before) == 1:
+            pipette.mix(repetitions=mix_before[0],
+                        volume=vol_ar[-1],
+                        location=source[0])
+        if len(mix_before) == 2:
+            pipette.mix(repetitions=mix_before[0],
+                        volume=mix_before[1],
+                        location=source[0])
+    pipette.aspirate(volume=vol_ar[-1], location=source[0])
+    if(delay_time_s):
+        pipette.air_gap(volume=0)
+        protocol.delay(seconds=delay_time_s)
+    pipette.dispense(volume=vol_ar[-1], location=dest[0])
+    if mix_after:
+        if len(mix_after) == 1:
+            pipette.mix(repetitions=mix_after[0], volume=vol_ar[-1])
+        if len(mix_after) == 2:
+            pipette.mix(repetitions=mix_after[0], volume=mix_after[1])
+
+    pipette.blow_out(location=dest[0])
+    if touch_tip:
+        pipette.touch_tip(radius=touch_tip[0],
+                          v_offset=touch_tip[1],
+                          speed=TOUCH_SPEED)
+    pipette.drop_tip()
